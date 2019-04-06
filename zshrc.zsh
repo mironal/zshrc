586bdbd429c402f2e279f4b700248fbca959fd14
mironal <m.yamaguchi@covelline.com>
2019-04-06 11:09:57 +0900

    hist_ignore_all_dups

--- zshrc.zsh
+++ zshrc.zsh
@@ -19,8 +19,7 @@ SAVEHIST=10000
 
 ## History command configuration
 setopt extended_history       # record timestamp of command in HISTFILE
-setopt hist_expire_dups_first # delete duplicates first when HISTFILE size exceeds HISTSIZE
-setopt hist_ignore_dups       # ignore duplicated commands history list
+setopt hist_ignore_all_dups
 setopt hist_ignore_space      # ignore commands that start with space
 setopt hist_verify            # show command with history expansion to user before running it
 setopt inc_append_history     # add commands to HISTFILE in order of execution
@@ -56,6 +55,9 @@ source '/Users/miro/bin/google-cloud-sdk/completion.zsh.inc'
 
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
+
 aliasList=("${(@f)$(alias)}")
 echo You can use this alias: ${aliasList[$RANDOM % ${#aliasList[@]} ]}
+
 
